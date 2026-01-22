9cfd93e3758db945b9c33853331090f0d57b329d
lealw0 <lea.lwolff@gmail.com>
2026-01-22 14:43:42 +0100

    cssV3

--- js/layout.js
+++ js/layout.js
@@ -21,16 +21,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const nav = document.createElement("nav");
   nav.classList.add("nav-buttons");
   nav.innerHTML = `
-    <a href="acceuil/acceuil.html" class="nav-btn">Accueil</a>
-    <a href="page2_V1.html" class="nav-btn">Événements</a>
-    <a href="formulaire/formulaire.html" class="nav-btn">Contact</a>
+    <a href="../gestion_projet/accueil/accueil.html" class="nav-btn">Accueil</a>
+    <a href="page2.html" class="nav-btn">Événements</a>
+    <a href="formulaire.html" class="nav-btn">Contact</a>
   `;
 
   /* ===== FOOTER ===== */
   const footer = document.createElement("footer");
   footer.innerHTML = `
     <div class="site-footer">
-      <p>© 2026 – Association environnementale locale</p>
+      <p>© 2026 – Diwallerien ar Vorlen</p>
+      <p> +33 2 29 29 29 29 | diwallerienarvorlen@nettoyons-la-rade.fr | 29 rue de la Mer, 29200 Brest</p>
       <p>Protection du littoral de la rade de Brest</p>
     </div>
   `;
